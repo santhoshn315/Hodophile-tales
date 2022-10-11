ad54cbcc5622b107c48c30cb5036ec36ff1bcd5b
Santhosh N <santhoshn315@gmail.com>
2022-10-12 01:50:58 +0530

    DB connection

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://santhoshn315:santhoshn315@cluster0.jdqftey.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, console.log(`server runnig on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
---
+++ Client/memories/src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+import useStyles from "./Styles";
+
+const Form = () => {
+  const classes = useStyles();
+  return <div>Form</div>;
+};
+
+export default Form;
