85af6f9a49a86e9f206a80b42bb944fc0a3e7b98
Santhosh N <santhoshn315@gmail.com>
2022-10-13 22:45:08 +0530

    redux part

--- Backend/controllers/posts.js
+++ Backend/controllers/posts.js
@@ -1,4 +1,7 @@
 import PostMessage from "../models/postMessage.js";
+import express from "express";
+
+const router = express.Router();
 
 export const getPosts = async (req, res) => {
   try {
@@ -9,13 +12,22 @@ export const getPosts = async (req, res) => {
   }
 };
 
-export const createPosts = async (req, res) => {
-  const post = req.body;
-  const newPost = new PostMessage(post);
+export const createPost = async (req, res) => {
+  const { title, message, selectedFile, creator, tags } = req.body;
+  const newPostMessage = new PostMessage({
+    title,
+    message,
+    selectedFile,
+    creator,
+    tags,
+  });
   try {
-    await newPost.save();
-    res.status(201).json(newPost);
+    await newPostMessage.save();
+
+    res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
 };
+
+export default router;
--- Client/memories/src/components/Posts/Posts.js
+++ Client/memories/src/components/Posts/Posts.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./Styles";
+import { useSelector } from "react-redux";
+
 const Posts = () => {
   const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
   return (
     <div>
       <h1>Posts</h1>
