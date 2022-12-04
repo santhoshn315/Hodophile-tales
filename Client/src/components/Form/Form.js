7655a17adc260342ce81e0b93c1f765758157a55
Santhosh N <santhoshn315@gmail.com>
2022-12-04 22:03:35 +0530

    changes

--- Client/src/components/Form/Form.js
+++ Client/src/components/Form/Form.js
@@ -52,7 +52,7 @@ const Form = ({ currentId, setCurrentId }) => {
     return (
       <Paper className={classes.paper} elevation={6}>
         <Typography variant="h6" align="center">
-          Please Sign In to create your own memories and like other's memories.
+          Please Sign In to create your own tales and like other's tales.
         </Typography>
       </Paper>
     );
