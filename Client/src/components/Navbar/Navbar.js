47ca17239a5d5afcabc3f8d01c9c17e9ff780fca
Santhosh N <santhoshn315@gmail.com>
2022-12-04 22:03:16 +0530

    changes

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
           variant="h2"
           align="center"
         >
-          Hodophile tales
+          Hodophile tales...
         </Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
